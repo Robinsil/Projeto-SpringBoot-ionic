c6fb1e65f8385e56cb77b13069777ccce97e8acd
Silva <robinson.r.silva@accenture.com>
2019-11-14 16:50:11 -0300

    Bônus: ajustando tamanho da imagem

--- src/main/java/com/projetoSpring/services/ImageService.java
+++ src/main/java/com/projetoSpring/services/ImageService.java
@@ -10,12 +10,11 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.projetoSpring.services.exepitions.FileException;
-
-
 
 @Service
 public class ImageService {
@@ -53,4 +52,23 @@ public class ImageService {
 			throw new FileException("Erro ao ler arquivo");
 		}
 	}
+	
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(
+			sourceImg, 
+			(sourceImg.getWidth()/2) - (min/2), 
+			(sourceImg.getHeight()/2) - (min/2), 
+			min, 
+			min);		
+	}
+	
+	public BufferedImage resize(BufferedImage sourceImg, int size) {
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
+	}
+	
 }
+
+
+
+
