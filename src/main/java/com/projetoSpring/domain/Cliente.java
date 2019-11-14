f0cf83b0d5669a03833fbed1fddb85170b533887
Silva <robinson.r.silva@accenture.com>
2019-11-14 15:54:04 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/projetoSpring/domain/Cliente.java
+++ src/main/java/com/projetoSpring/domain/Cliente.java
@@ -66,8 +66,6 @@ public class Cliente implements Serializable{
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido>pedidos = new ArrayList<>();
 	
-    private String imageUrl;
-	
 	public Cliente() {
 		
 		addPerfil(Perfil.CLIENTE);
@@ -192,15 +190,5 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-
-
 
 }
