c48b55a8d13c3896130d12a2732bd8dc557fa884
Silva <robinson.r.silva@accenture.com>
2019-11-15 12:43:28 -0300

    Endpoint para buscar cliente por email

--- src/main/java/com/projetoSpring/services/ClienteService.java
+++ src/main/java/com/projetoSpring/services/ClienteService.java
@@ -31,7 +31,7 @@ import com.projetoSpring.services.exepitions.ObjectNotFounExepition;
 
 @Service
 public class ClienteService {
-	
+
 	@Transactional
 	public Cliente insert(Cliente obj) {
 
@@ -40,56 +40,48 @@ public class ClienteService {
 		enderecoRepository.saveAll(obj.getEnderecos());
 		return obj;
 	}
-	
-	
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder pe;
-	
+
 	@Autowired
 	private S3Service s3Service;
-	
+
 	@Autowired
 	private ImageService imageService;
-	
+
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
-	
+
 	@Value("${img.profile.size}")
 	private Integer size;
-	
-	
-	
+
 	public Cliente find(Integer id) {
-		
+
 		UserSS user = UserService.authenticated();
-		if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
 			throw new AuthorizationException("Acesso negado");
 		}
-		
+
 		Optional<Cliente> obj = clienteRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFounExepition(
-				"Objeto não encontrado com id = "+ id + " Tipo " + Cliente.class.getName()));
+				"Objeto não encontrado com id = " + id + " Tipo " + Cliente.class.getName()));
 	}
-	
-	
+
 	public Cliente update(Cliente obj) {
-		
-		Cliente newObj =find(obj.getId());
-		updateData(newObj,obj);
-		
+
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+
 		return clienteRepository.save(newObj);
 	}
 
-	
-	
-	
 	public void delete(Integer id) {
 
 		find(id);
@@ -107,70 +99,82 @@ public class ClienteService {
 	}
 
 	public List<Cliente> FindAll() {
-		
+
 		return clienteRepository.findAll();
 	}
-	
-	
-	public Page<Cliente>findPage(Integer page,Integer linesPages,String orderBy,String direction){
-		
-		PageRequest pageRequest = PageRequest.of(page, linesPages,Direction.valueOf(direction), orderBy);
-		
+
+	public Page<Cliente> findPage(Integer page, Integer linesPages, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPages, Direction.valueOf(direction), orderBy);
+
 		return clienteRepository.findAll(pageRequest);
-		
+
 	}
-	
+
 	public Cliente fromDTO(ClienteNewDTO objDTO) {
-		
-		Cliente cli = new Cliente(null,objDTO.getNome(),objDTO.getEmail(),objDTO.getCPFouCNPJ(),TipoCliente.toEnum(objDTO.getTipo()),pe.encode(objDTO.getSenha()));
-		Cidade cid = new Cidade(objDTO.getCidadeId(),null,null);
-		Endereco end = new Endereco(null,objDTO.getLogradouro(),objDTO.getNumero(),objDTO.getComplemento(),objDTO.getBairro(),objDTO.getCep(), cli, cid);
+
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCPFouCNPJ(),
+				TipoCliente.toEnum(objDTO.getTipo()), pe.encode(objDTO.getSenha()));
+		Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(),
+				objDTO.getBairro(), objDTO.getCep(), cli, cid);
 		cli.getEnderecos().add(end);
 		cli.getTelefones().add(objDTO.getTelefone1());
-		
-		if(objDTO.getTelefone2() != null) {
+
+		if (objDTO.getTelefone2() != null) {
 			cli.getTelefones().add(objDTO.getTelefone2());
 		}
-	
-		if(objDTO.getTelefone3()!= null) {
+
+		if (objDTO.getTelefone3() != null) {
 			cli.getTelefones().add(objDTO.getTelefone3());
 		}
-		 return cli;
-		
+		return cli;
+
 	}
-	
-	
-	
+
 	public Cliente fromDTO(ClienteDTO objDTO) {
-		
-		return new Cliente(objDTO.getId(),objDTO.getNome(),objDTO.getEmail(),null,null,null);
+
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null, null);
 	}
-	
+
 	private void updateData(Cliente newObj, Cliente obj) {
-		
+
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
-		
+
 	}
-	
+
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		
+
 		UserSS user = UserService.authenticated();
-		
-		if(user == null) {
+
+		if (user == null) {
 			throw new AuthorizationException("Acesso Negado");
 		}
-	
+
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
-		
+
 		jpgImage = imageService.cropSquare(jpgImage);
 		jpgImage = imageService.resize(jpgImage, size);
-		
+
 		String fileName = prefix + user.getId() + ".jpg";
-		
+
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
-			
-		
+
+	}
+
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
+		Cliente obj = clienteRepository.findByEmail(email);
+		if (obj == null) {
+			throw new ObjectNotFounExepition(
+					"Objeto não encontrado! Id: " + user.getId() + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
 	}
 
 }
